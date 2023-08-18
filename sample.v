77549ead729a3a53acc476b69c570348a94badf1
vzhn <vzhilin.me@gmail.com>
2023-08-18 22:04:45 +0200

    draft beta reduction

--- sample.v
+++ sample.v
@@ -1,4 +1,4 @@
-
+Require Import Coq.Init.Nat.
 
 Inductive LambdaTerm : Type :=
   | variable (n:  nat)
@@ -8,10 +8,29 @@ Inductive LambdaTerm : Type :=
 
 Fixpoint height (t: LambdaTerm) : nat :=
   match t with
-    | variable n  => 1
+    | variable n => 1
     | app a b  => 1 + (max (height a) (height b))
     | abst _ b => 1 + (height b)
   end.
+
+Fixpoint substitution (t: LambdaTerm) (n: nat) (replacement: LambdaTerm): LambdaTerm := 
+  match t with
+    | variable v => if (v =? n) then replacement else t
+    | app a b => app (substitution a n replacement) (substitution b n replacement)
+    | abst v b => if (v =? n) then t else abst v (substitution b n replacement)
+  end.
+
+
+Definition has_redex (t: LambdaTerm): bool :=
+  end.
+
+Fixpoint beta (t: LambdaTerm): LambdaTerm :=
+  match t with
+    | variable v => t
+    | app a b => t
+    | abst v b => abst v (beta b)
+  end.
+
 
 Compute (
   height 
