1a6b2e9c6bd90774eda4b0088e5b37144af2b542
vzhn <vzhilin.me@gmail.com>
2023-09-07 19:17:32 +0200

    working beta

--- debrujin.v
+++ debrujin.v
@@ -21,22 +21,16 @@ match a, b with
 | _, _ => false
 end.
 
-Definition s_comb := (abst (abst (abst   (app (app (var 2) (var 0)) (app (var 1) (var 0)))))).
+Definition s_comb := (abst (abst (abst (app (app (var 2) (var 0)) (app (var 1) (var 0)))))).
 
 Definition k_comb := (abst (abst (var 1))).
-
-(* 
-Fixpoint replace (depth: nat) (t: LambdaTerm) (r: LambdaTerm): LambdaTerm :=
-match t with
-| var n =>
-| app a b =>
-| abst b => replace (S depth) b r
-end. *)
 
 Fixpoint dec_free_vars (t: LambdaTerm) (depth: nat): LambdaTerm :=
 match t with  
 | var 0 => t
-| var (S n) => if (depth <=? n) then var n else t
+| var (S n) => if (depth <=? n) 
+               then var n
+               else t
 | app a b => app (dec_free_vars a depth) (dec_free_vars b depth)
 | abst a => abst (dec_free_vars a (S depth))
 end.
@@ -44,30 +38,28 @@ end.
 (* depth = 0 -> 'every variable is free' *)
 (* depth = 1 -> 'every variable except 0 is free' *)
 (* depth = 2 -> 'every variable except 0, 1 is free' *)
-Fixpoint inc_free_vars (t: LambdaTerm) (depth: nat): LambdaTerm :=
+Fixpoint inc_free_vars (t: LambdaTerm) (depth: nat) (amount: nat): LambdaTerm :=
 match t with
 | var n => if (depth <=? n)
-            then var (n + 1)
+            then var (n + amount)
             else t
-| app a b => app (inc_free_vars a depth) (inc_free_vars b depth)
-| abst a => abst (inc_free_vars a (S depth))
+| app a b => app (inc_free_vars a depth amount) (inc_free_vars b depth amount)
+| abst a => abst (inc_free_vars a (S depth) amount)
 end.
 
 (* starting from depth = 0 *)
 Fixpoint replace_instances (a b: LambdaTerm) (depth: nat) :=
 match a with
 | var v => if (v =? depth) 
-           then b 
-           else if (depth <? v)
-                then var (pred v)
-                else a
+           then (inc_free_vars b 0 (1 + depth))
+           else var v
 | app l r => app (replace_instances l b depth) (replace_instances r b depth)
 | abst body => abst (replace_instances body b (S depth))
 end.
 
 (*
   From Wiki:
-  beta-reduction (λ M) N 
+  beta-reduction (\u03bb M) N 
   1. find bound instances in M
   2. decrement free variables of M
   3. replace bound instances of M with N, incrementing free variables of N
@@ -80,7 +72,7 @@ end.
 
 Fixpoint beta (t: LambdaTerm): LambdaTerm :=
 match t with
-| (app (abst a) b) => (dec_free_vars (replace_instances a (inc_free_vars b 0) 0) 0)
+| (app (abst a) b) => (dec_free_vars (replace_instances a b 0) 0)
 | app a b => let ba := beta a in
                if (lambda_eq a ba)
                 then app a (beta b)
@@ -88,58 +80,86 @@ match t with
 | _ => t
 end.
 
+Example beta_example_007: 
+ beta (app (abst n3) n6) = n2.
+Proof. simpl. reflexivity. Qed.
+
+
 Theorem inv_fv_simple_v1:
  forall (t: LambdaTerm) (depth: nat),
- (dec_free_vars (inc_free_vars (var 0) depth) depth) = (var 0).
+ (dec_free_vars (inc_free_vars (var 0) depth 1) depth) = (var 0).
 Proof.
  induction depth.
   - simpl. reflexivity.
   - simpl. reflexivity.
 Qed.
 
-Theorem inv_fv_simple_v2:
- forall (t: LambdaTerm) (depth: nat),
- (dec_free_vars (inc_free_vars t depth) depth) = t.
-Proof.
-Admitted.
-
-Example inc_000: inc_free_vars (var 0) 0 = var 1.
+Example inc_000: inc_free_vars (var 0) 0 1 = var 1.
 Proof. simpl. reflexivity. Qed.
 
-Example inc_001: inc_free_vars (var 0) 1 = var 0.
+Example inc_001: inc_free_vars (var 0) 1 1 = var 0.
 Proof. simpl. reflexivity. Qed.
 
-Example inc_002: inc_free_vars (var 1) 1 = var 2.
+Example inc_002: inc_free_vars (var 1) 1 1 = var 2.
 Proof. simpl. reflexivity. Qed.
 
-Example inc_003: inc_free_vars (abst (var 0)) 0 = (abst (var 0)).
+Example inc_003: inc_free_vars (abst (var 0)) 0 1 = (abst (var 0)).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_004: inc_free_vars (abst (var 1)) 0 = (abst (var 2)).
+Example inc_004: inc_free_vars (abst (var 1)) 0 1 = (abst (var 2)).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_005: inc_free_vars (abst (abst (var 2))) 0 = (abst (abst (var 3))).
+Example inc_005: inc_free_vars (abst (abst (var 2))) 0 1 = (abst (abst (var 3))).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_006: inc_free_vars (abst (abst (var 3))) 0 = (abst (abst (var 4))).
+Example inc_006: inc_free_vars (abst (abst (var 3))) 0 1 = (abst (abst (var 4))).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_007: inc_free_vars (abst (abst (var 1))) 0 = (abst (abst (var 1))).
+Example inc_007: inc_free_vars (abst (abst (var 1))) 0 1 = (abst (abst (var 1))).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_008: inc_free_vars (abst (abst (var 2))) 1 = (abst (abst (var 2))).
+Example inc_008: inc_free_vars (abst (abst (var 2))) 1 1 = (abst (abst (var 2))).
 Proof. simpl. reflexivity. Qed.
 
-Example inc_009: inc_free_vars (abst (abst (var 2))) 0 = (abst (abst (var 3))).
+Example inc_009: inc_free_vars (abst (abst (var 2))) 0 1 = (abst (abst (var 3))).
 Proof. simpl. reflexivity. Qed.
 
 Example beta_example_001: beta (app (abst (var 0)) (app (var 0) (var 1))) = (app (var 0) (var 1)).
 Proof. simpl. reflexivity. Qed.
 
+Example beta_example_006: 
+ beta (app (abst (abst (app (var 0) (var 1)))) (var 0)) = 
+      (abst (app (var 0) (var 1))).
+Proof. simpl. reflexivity. Qed.
+
+
+Example beta_example_002:
+ beta (app (abst (abst (abst (abst (var 3))))) (abst (app (var 0) (var 1)))) =
+ (abst (abst (abst (abst (app (var 0) (var 4)))))).
+Proof. simpl. reflexivity. Qed.
+
+
+Example beta_example_005: 
+ beta (app (abst (abst n3)) n6) = abst n2.
+Proof. simpl. reflexivity. Qed.
+
+
+
 Example beta_example_004: 
- beta (app (abst (abst (abst (abst ))))
-           (abst (abst (app (app n3 n1) (abst (app n0 n2)))))
- ).
+ beta (app (abst (abst (app n3 n1))) n3) = 
+ abst (app n2 n4).
+Proof. simpl. reflexivity. Qed.
+
+Example beta_example_008: 
+ beta (app (abst (abst n3)) n6) = 
+ abst n2.
+Proof. simpl. reflexivity. Qed.
+
+Example beta_example_009: 
+ beta (app (abst (abst (app (app n3 n1) (abst (app n0 n2)))))
+           n3) = 
+ abst (app (app n2 n4) 
+           (abst (app n0 n5))).
 Proof. simpl. reflexivity. Qed.
 
 Example beta_example_003: 
